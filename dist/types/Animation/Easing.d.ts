41e9816d7cec84008bd8eaf8a52f4099ffa69fde
Christian Broms <cb@christianbroms.com>
2019-08-09 11:40:04 -0500

    fix package entry and release 0.1.5

--- dist/types/Animation/Easing.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Easing functions, originally from Rober Penner, javascript implementation from
- * https://github.com/chenglou/tween-functions, typescript port unique to this lib
- */
-export declare const Easing: any;
--- dist/types/Error.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Create a new error and print it to the console
- * @param newError - error string to print
- * @returns - false
- */
-export declare const Error: (newError: string) => boolean;
