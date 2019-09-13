a17848fea210a7066411449e9d3395002bcdd6d6
CBR0MS <cb@christianbroms.com>
2019-09-13 16:53:10 -0400

    work on ex3

--- docs/_media/examples/ex3/sketch.js
+++ docs/_media/examples/ex3/sketch.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("testCanvas");
 // canvas size
 let w, h;
+let mid = 150;
 
 const setSize = () => {
   w = window.innerWidth;
@@ -44,42 +45,64 @@ const start = async () => {
   // load the font
   const font = await fg.loadFont("assets/CrimsonText-Regular.ttf");
 
-  // background text
-  const tb = new V4.TextBox({
+  const aboveTb = new V4.TextBox({
     font: font,
-    position: { x: 0, y: h / 2 + 150 },
-    size: { h: 300, w: 1000 },
+    position: { x: 0, y: h / 2 - mid / 2 },
+    size: { h: h / 2 - mid / 2, w: w },
     fontSize: 48,
     color: "white",
-    backgroundColor: "transparent",
+    backgroundColor: "red",
+    horizontalAlign: "LEFT",
+    verticalAlign: "BOTTOM",
+  });
+  rq.push(aboveTb.renderer);
+
+  const midTb = new V4.TextBox({
+    font: font,
+    position: { x: 0, y: h / 2 + mid / 2 },
+    size: { h: mid, w: w },
+    fontSize: 48,
+    color: "white",
+    backgroundColor: "blue",
+    horizontalAlign: "LEFT",
+    verticalAlign: "TOP",
+  });
+  rq.push(midTb.renderer);
+
+  const belowTb = new V4.TextBox({
+    font: font,
+    position: { x: 0, y: h },
+    size: { h: h / 2 - mid / 2, w: w },
+    fontSize: 48,
+    color: "white",
+    backgroundColor: "green",
     horizontalAlign: "LEFT",
     verticalAlign: "CENTER",
   });
-
+  rq.push(belowTb.renderer);
   let pos = 0;
 
-  const animate = () => {
-    // set the new text and create the next animation
-    tb.text(words[pos]);
-    const anim = new V4.Animation(
-      tb,
-      {
-        position: { x: pos * 100, y: h / 2 + 150 },
-        fontSize: makeSize(words[pos]),
-      },
-      makeTime(words[pos]),
-      "easeInSine",
-    );
+  // const animate = () => {
+  //   // set the new text and create the next animation
+  //   tb.text(words[pos]);
+  //   const anim = new V4.Animation(
+  //     tb,
+  //     {
+  //       position: { x: pos * 100, y: h / 2 + 150 },
+  //     },
+  //     makeTime(words[pos]),
+  //     "easeInSine",
+  //   );
 
-    // add the animation to the queue and repeat on done
-    rq.push(anim.renderer, () => {
-      if (pos + 1 === words.length) pos = 0;
-      else pos++;
-      animate();
-    });
-  };
+  //   // add the animation to the queue and repeat on done
+  //   rq.push(anim.renderer, () => {
+  //     if (pos + 1 === words.length) pos = 0;
+  //     else pos++;
+  //     animate();
+  //   });
+  // };
 
-  animate();
+  // animate();
 
   loop.framesPerSecond(60);
   loop.startLoop();
